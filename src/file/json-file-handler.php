a5eb1161ffe2a1a1434d9c77b39942bd8d0e5fb1
Simon Cabos <36542336+simUf0@users.noreply.github.com>
2020-03-09 11:17:41 +0100

    Add files via upload

---
+++ src/file/json-file-handler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SimFwk2\File;
+
+/**
+ * Json file handler
+ * 
+ * @author Simon Cabos
+ * @version 1.0.3
+ * @copyright 2020 Simon Cabos
+ * @licence GPL - http://www.gnu.org/licenses/gpl-3.0.html
+ */
+final class JsonFileHandler extends FileHandler {
+  
+  /**
+   * Returns the content of the json file. File must be opened before.
+   * @return mixed[] The decoded json file's content.
+   */
+  public function read (): array {
+    return json_decode(parent::read(), true);
+  }
+
+  /**
+   * Write content into a json file.
+   * @param mixed $content The json's content to write.
+   */
+  public function writeJson ($content): void {
+    parent::write(json_encode($content));
+  }
+}
